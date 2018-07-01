192569213c4aa3cf22c1a8a233936cd50395b9a9
Tatiane Carvalho <tsancarvalho@gmail.com>
2018-07-01 13:19:38 -0300

    Melhorias no feed

---
+++ client/Feed	/Feed.js
@@ -0,0 +1,11 @@
+Template.Feed.events({
+	"submit form": function(evento, template) {
+		evento.preventDefault();
+		var textoDoFormulario = evento.target.texto.value;
+		console.log(textoDoFormulario);
+		Posts.insert({
+			texto: textoDoFormulario
+		})
+		evento.target.texto.value="";
+	}
+});
